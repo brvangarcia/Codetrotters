988c1afa5749c463a7ac006935a26900f6b85fb1
Bryan <bexplorador1@gmail.com>
2017-08-03 13:46:51 -0400

    Create app.js

---
+++ Day 1/JS/app.js
@@ -0,0 +1,10 @@
+function sub() {
+  var num = prompt("Numer:");
+  if (num >= 0) {
+    for ( num >= 0; num = num - 1;) {
+      console.log(num)
+    }
+  } else {
+      console.log("false");
+  }
+}
